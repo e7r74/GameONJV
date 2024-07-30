9afda6287b0848382d48a86d7c8b9e067d0d2a14
Yerkhan <127944702+e7r74@users.noreply.github.com>
2024-07-30 15:48:15 +0500

    Add files via upload

---
+++ src/main/java/com/pclup/Pclub/PclubApplication.java
@@ -0,0 +1,13 @@
+package com.pclup.Pclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PclubApplication.class, args);
+	}
+
+}
